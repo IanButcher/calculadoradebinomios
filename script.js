5f1843d18c26a6ca22d5e88c92d388095132c050
IanButcher <115805201+IanButcher@users.noreply.github.com>
2024-04-24 11:06:44 -0300

    script

---
+++ script.js
@@ -0,0 +1,37 @@
+// Variables
+const cuadradoFormula = document.getElementById('cuadrado');
+const cuboFormula = document.getElementById('cubo');
+const solucionTitulo = document.getElementById('solucion');
+const btnBotonsito = document.getElementById('boton');
+
+// Calcular
+btnBotonsito.addEventListener("click", function(){
+    // Retrieve input values
+    const aInput = parseFloat(document.getElementById('aNumberInput').value);
+    const bInput = parseFloat(document.getElementById('bNumberInput').value);
+    const exponenteInput = parseInt(document.getElementById('exponenteNumberInput').value);
+
+    // Type of exponente
+    if (exponenteInput === 2) {
+        // Modifying formula
+        const cuadrado = `(${aInput} + ${bInput})2 = ${aInput}2 + 2(${aInput})(${bInput}) + ${bInput}2`;
+        cuadradoFormula.textContent = cuadrado;
+        console.log(cuadrado)
+
+        // Solving operation
+        const solucion = aInput ** 2 + 2 * aInput * bInput + bInput ** 2;
+        solucionTitulo.textContent = `Solución: ${solucion}`;     
+
+    } 
+    else if (exponenteInput === 3) {
+        // Modifying formula
+        const cubo = `(${aInput} + ${bInput})3 = ${aInput}3 + 3(${aInput})2(${bInput}) + 3(${bInput})2(${aInput}) + ${bInput}3`;
+        cuboFormula.textContent = cubo;
+        console.log(cubo)
+
+        // Solving operation
+        const solucion = aInput ** 3 + 3 * aInput** 2 * bInput + 3 * bInput ** 2 * aInput + bInput ** 3;
+        solucionTitulo.textContent = `Solución: ${solucion}`;
+    }
+    console.log(solucion)
+});
