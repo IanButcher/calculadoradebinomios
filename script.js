0c3ea5d9e2645f744e9dee279af89c0739d04eb7
IanButcher <iancorradibutcher@gmail.com>
2024-04-30 14:49:43 -0300

    Error Handling, Fake DataBase, DarkMode functionality

--- script.js
+++ script.js
@@ -1,40 +1,73 @@
 // Variables
-const cuadradoFormula = document.getElementById('cuadrado');
-const cuboFormula = document.getElementById('cubo');
-const solucionTitulo = document.getElementById('solucion');
-const btnBotonsito = document.getElementById('boton');
+const cuadradoFormula = document.querySelector('#cuadrado')
+const cuboFormula = document.querySelector('#cubo')
+const solucionTitulo = document.querySelector('#solucion')
+const btnBotonsito = document.querySelector('#boton')
+const cuadradosHechos = document.querySelector('.cuadradosHechos')
+const cubosHechos = document.querySelector('.cubosHechos')
 
-// Calcular
-btnBotonsito.addEventListener("click", function(){
+// Solve
+btnBotonsito.addEventListener("click", ()=>{
+
     // Retrieve input values
-    const aInput = parseFloat(document.getElementById('aNumberInput').value);
-    const bInput = parseFloat(document.getElementById('bNumberInput').value);
-    const exponenteInput = parseInt(document.getElementById('exponenteNumberInput').value);
+    const aInput = parseFloat(document.querySelector('#aNumberInput').value)
+    const bInput = parseFloat(document.querySelector('#bNumberInput').value)
+    const exponenteInput = parseInt(document.getElementById('exponenteNumberInput').value)
 
+    // Handling NaN bugs
+    const allInputs = document.querySelectorAll("input")
+    for (const diputado of allInputs){
+        if(!diputado.value){
+            alert("El valor de A y/o B no puede ser nulo o cadena de caracteres. Por favor, ingrese un numero")
+            return
+        }
+    }
     // Type of exponente
     if (exponenteInput === 2) {
         // Modifying formula
-        const cuadrado = `(${aInput} + ${bInput})2 = ${aInput}2 + 2(${aInput})(${bInput}) + ${bInput}2`;
-        cuadradoFormula.textContent = cuadrado;
-        console.log(cuadrado)
+        const cuadrado = `(${aInput} + ${bInput}) <sup>2</sup> = ${aInput}<sup>2</sup> + 2(${aInput})(${bInput}) + ${bInput} <sup>2</sup>`
+        cuadradoFormula.innerHTML = cuadrado
 
         // Solving operation
-        const solucion = aInput ** 2 + 2 * aInput * bInput + bInput ** 2;
-        solucionTitulo.textContent = `Solución: ${solucion}`;     
+        const solucion = aInput ** 2 + 2 * aInput * bInput + bInput ** 2
+        solucionTitulo.textContent = `Solución: ${solucion}`
 
+        // Adding it to cuadradosHechos
+        const cuadradoResult = `<h4>${cuadrado}</h4> = ${solucion}`
+        cuadradosHechos.insertAdjacentHTML('beforeend', cuadradoResult)
     } 
     else if (exponenteInput === 3) {
         // Modifying formula
-        const cubo = `(${aInput} + ${bInput})3 = ${aInput}3 + 3(${aInput})2(${bInput}) + 3(${bInput})2(${aInput}) + ${bInput}3`;
-        cuboFormula.textContent = cubo;
-        console.log(cubo)
+        const cubo = `(${aInput} + ${bInput})<sup>3</sup> = ${aInput}<sup>3</sup> + 3(${aInput})<sup>2</sup>(${bInput}) + 3(${bInput})<sup>2</sup>(${aInput}) + ${bInput}<sup>3</sup>`
+        cuboFormula.innerHTML = cubo
 
         // Solving operation
-        const solucion = aInput ** 3 + 3 * aInput** 2 * bInput + 3 * bInput ** 2 * aInput + bInput ** 3;
-        solucionTitulo.textContent = `Solución: ${solucion}`;
+        const solucion = aInput ** 3 + 3 * aInput** 2 * bInput + 3 * bInput ** 2 * aInput + bInput ** 3
+        solucionTitulo.textContent = `Solución: ${solucion}`
+
+            // Adding it to cubosHechos
+            const cuboResult = `<h4>${cubo}</h4> = ${solucion}`
+            cubosHechos.insertAdjacentHTML('beforeend', cuboResult)
     }
     else {
         alert("No se hacerlo con ese exponente perdon uwu")
-    }
-    console.log(solucion)
+    }        
 });
+
+
+// DarkMode btn
+const darkModeBtn = document.querySelector('#darkMode')
+const allDivs = document.querySelectorAll("div")
+
+
+darkModeBtn.addEventListener("click", ()=>{
+    document.body.classList.toggle('DARKMODE')
+    for (const div of allDivs) {
+        //if (div.classList.contains === "ignoreDiv"){
+        //     console.log("Ignoramos este div XD no encontre una forma mejor")
+        //}
+        //else{
+        div.classList.toggle('DARKMODE')
+        //}
+    } 
+})
